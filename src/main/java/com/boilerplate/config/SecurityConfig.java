f9c89952b91967594baac69064ff8cba6568c9ad
inh3011 <inh3011@gmail.com>
2024-05-14 17:04:15 +0900

    feat: git --set-upstream origin develop

--- src/main/java/com/boilerplate/config/SecurityConfig.java
+++ src/main/java/com/boilerplate/config/SecurityConfig.java
@@ -15,7 +15,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-
         http.authorizeHttpRequests(authorizeRequests ->
                 authorizeRequests.anyRequest().permitAll());
 
