6e1d752cbf5a576b305ea6c175ac71807a08a085
inh3011 <inh3011@gmail.com>
2024-05-14 17:01:55 +0900

    feat: git config test

--- src/main/java/com/boilerplate/config/SecurityConfig.java
+++ src/main/java/com/boilerplate/config/SecurityConfig.java
@@ -15,6 +15,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+
         http.authorizeHttpRequests(authorizeRequests ->
                 authorizeRequests.anyRequest().permitAll());
 
