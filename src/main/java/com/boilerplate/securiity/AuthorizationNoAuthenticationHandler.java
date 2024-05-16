e9fca172a839c88eee09a102efbbe6d4c5270859
inh3011 <inh3011@gmail.com>
2024-05-16 17:34:16 +0900

    feat: Spring Security JWT 큰틀 생성

---
+++ src/main/java/com/boilerplate/securiity/AuthorizationNoAuthenticationHandler.java
@@ -0,0 +1,22 @@
+package com.boilerplate.securiity;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AuthorizationNoAuthenticationHandler implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+
+    }
+}
