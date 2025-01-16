5c2b4de958d2a8b7cb1216c57dfd8c27c9e36ef3
inh3011 <inh3011@gmail.com>
2025-01-16 14:38:25 +0900

    feat: audit log entity 수정

--- src/main/java/com/boilerplate/infrastructure/entity/AuditLog.java
+++ src/main/java/com/boilerplate/infrastructure/entity/AuditLog.java
@@ -9,7 +9,6 @@ import java.time.LocalDateTime;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Setter
 @Entity
 @Table(name = "audit_logs")
 public class AuditLog {
@@ -18,17 +17,42 @@ public class AuditLog {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "action")
     private Action action;
-    private String entityName;
-    private Long entityId;
-    private String username;
-    private LocalDateTime timestamp;
 
-    public AuditLog(Action action, String entityName, Long entityId, String username) {
+    @Column(name = "entity_name")
+    private String entityName;
+
+    @Column(name = "entity_id")
+    private Long entityId;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "request", columnDefinition = "TEXT")
+    private String request;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Builder
+    public AuditLog(
+            Action action,
+            String entityName,
+            Long entityId,
+            String username,
+            String request,
+            Long userId
+    ) {
         this.action = action;
         this.entityName = entityName;
         this.entityId = entityId;
         this.username = username;
-        this.timestamp = LocalDateTime.now();
+        this.userId = userId;
+        this.request = request;
+        this.createdAt = LocalDateTime.now();
     }
 }
