c84b47b7e14745b8c746f34316074e7771c94a5b
inh3011 <inh3011@gmail.com>
2025-01-16 14:38:46 +0900

    feat: audit_log repository 생성

---
+++ src/main/java/com/boilerplate/infrastructure/repository/AuditLogRepository.java
@@ -0,0 +1,7 @@
+package com.boilerplate.infrastructure.repository;
+
+import com.boilerplate.infrastructure.entity.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+}
